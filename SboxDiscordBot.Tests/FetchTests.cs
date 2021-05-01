03be8a9076e0e7cffe777477fc3b99f2f49b3d32
Alex <alex@gu3.me>
2021-05-01 13:44:30 +0100

    Add working tests for Fetch Delete DotSettings file

--- SboxDiscordBot.Tests/FetchTests.cs
+++ SboxDiscordBot.Tests/FetchTests.cs
@@ -1,50 +1,50 @@
-// using System.Collections.Generic;
-// using System.Threading.Tasks;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-//
-// namespace SboxDiscordBot.Tests
-// {
-//     [TestClass]
-//     public class FetchTests
-//     {
-//         [TestMethod]
-//         public void FetchJsonTest()
-//         {
-//             Request.Fetch("https://jsonplaceholder.typicode.com/todos/1").Then(response =>
-//             {
-//                 var jsonResponse = response.Json();
-//                 var desiredObject = new Dictionary<string, string>
-//                 {
-//                     {"userId", "1"},
-//                     {"id", "1"},
-//                     {"title", "delectus aut autem"},
-//                     {"completed", "false"}
-//                 };
-//                 Assert.IsTrue(jsonResponse == desiredObject);
-//             }).Catch(exception => { Assert.Fail(); });
-//         }
-//
-//         [TestMethod]
-//         public void FetchTextTest()
-//         {
-//             Request.Fetch("https://jsonplaceholder.typicode.com/todos/1").Then(response =>
-//             {
-//                 var textResponse = response.Text();
-//                 Assert.IsTrue(textResponse ==
-//                               "{\"userId\": 1,\n  \"id\": 1,\n  \"title\": \"delectus aut autem\",\n  \"completed\": false\n}");
-//             }).Catch(exception => { Assert.Fail(); });
-//         }
-//
-//         [TestMethod]
-//         public void FacepunchApiTest()
-//         {
-//             Task.Run(async () =>
-//             {
-//                 Request.Fetch("https://apix.facepunch.com/api/sbox/menu/index").Then(response =>
-//                 {
-//                     Assert.IsTrue(response.Text().Length > 0);
-//                 }).Catch(exception => { Assert.Fail(); });
-//             });
-//         }
-//     }
-// }
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Frameworks;
+
+namespace SboxDiscordBot.Tests
+{
+    [TestClass]
+    public class FetchTests
+    {
+        [TestMethod]
+        public void FetchJsonTest()
+        {
+            Request.Fetch("https://jsonplaceholder.typicode.com/todos/1").ContinueWith(task =>
+            {
+                if (task.Exception != null)
+                    Assert.Fail();
+                
+                var response = task.Result;
+                var jsonResponse = response.Json();
+                var desiredObject = new Dictionary<string, string>
+                {
+                    {"userId", "1"},
+                    {"id", "1"},
+                    {"title", "delectus aut autem"},
+                    {"completed", "false"}
+                };
+                
+                Assert.IsTrue(jsonResponse.Count == desiredObject.Count && !jsonResponse.Except(desiredObject).Any());
+            }).GetAwaiter().GetResult();
+        }
+        
+        [TestMethod]
+        public void FetchTextTest()
+        {
+            Request.Fetch("https://jsonplaceholder.typicode.com/todos/1").ContinueWith(task =>
+            {
+                if (task.Exception != null)
+                    Assert.Fail();
+                
+                var response = task.Result;
+                
+                var textResponse = response.Text();
+                Assert.IsTrue(textResponse ==
+                              "{\n  \"userId\": 1,\n  \"id\": 1,\n  \"title\": \"delectus aut autem\",\n  \"completed\": false\n}");
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
